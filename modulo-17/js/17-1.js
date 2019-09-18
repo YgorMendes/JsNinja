97f3d29f532e5504ead8db61f9ae34cd7f7f4d87
Ygor <ygortmendes@gmail.com>
2019-09-18 20:35:28 -0300

    add 17.1

---
+++ modulo-17/js/17-1.js
@@ -0,0 +1,8 @@
+(function(){
+  // Manipular strings
+   
+  let texto = 'aslk jdlaks jgdla skdglak sdla yg adja ldggyggjp kr eyneurnery9 2y'
+  console.log(texto.match(/a/g)); // busca os caracteres entre as barras, e o 'g'
+  //depois da barra é uma função do 'match', que faz buscar todos os caracteres dentro da
+  // barra 'g' de 'global'
+})();
