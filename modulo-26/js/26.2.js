90044fd334920a2f7629a02c2cb9933acf7544fe
Ygor <ygortmendes@gmail.com>
2019-11-13 20:23:58 -0300

    add modulo-29

--- modulo-26/js/26.2.js
+++ modulo-26/js/26.2.js
@@ -13,7 +13,7 @@
   // elementos html é tipo 1
   // text é tipo 3
   // comments tipo 8
-  // documentFragment = 22
+  // documentFragment = 11
 
   console.log($tituloPrincipal.firstChild.nodeValue); // mostra o valor da tag
   console.log($tituloPrincipal.firstChild.nodeName); // mostra o tipo, c for tag vai ser o nome dela, c for somente um texto vaui ser apenas #text, e se for comentario vai ser #comit
